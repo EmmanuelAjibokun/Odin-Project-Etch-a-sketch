e6f84e2641c1b26c1a5cecbb35d330dcece60c1b
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-25 19:17:48 +0100

    display div class of children on the screen

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('manny')
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+div.classList.add('children');
+container.appendChild(div);
+console.log('manny');
+console.log(container);
