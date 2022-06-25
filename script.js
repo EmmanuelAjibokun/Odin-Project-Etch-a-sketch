e3305a0bfdc6e9beb96d279d1a9187cab84bfe3c
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-25 22:54:20 +0100

    create function that receive arguments to increase div along Y-axis and X-axis

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.querySelector('.container');
 
-function AppendElements(htmlStringList) {
-  for (let i = 0;i < htmlStringList;++i) {
+function AppendElements(yNodeCount, xNodeCount) {
+  for (let i = 0;i < yNodeCount; ++i) {
     var div = document.createElement('div');
     div.classList.add('children');
     container.appendChild(div);
+    for(let j = 0; j < xNodeCount; ++j) {
+      var div2 = document.createElement('div');
+      div2.classList.add('xChildren');
+      div.appendChild(div2);
+    }
   }
 }
 
-AppendElements(6)
+AppendElements(10, 20)
