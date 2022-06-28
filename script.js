62236bd99c0eef93f93148d87d9886f9b0b0f67b
manny300 <96999656+manny300@users.noreply.github.com>
2022-06-28 08:40:53 +0100

    center container div

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const container = document.querySelector('.container');
 
 function AppendElements(yNodeCount, xNodeCount) {
   for (let i = 0;i < yNodeCount; ++i) {
-    var div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('children');
     container.appendChild(div);
     for(let j = 0; j < xNodeCount; ++j) {
-      var div2 = document.createElement('div');
+      let div2 = document.createElement('div');
       div2.classList.add('xChildren');
       div.appendChild(div2);
     }
